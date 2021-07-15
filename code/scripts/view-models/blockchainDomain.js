95e33fbb7a1340bf53719932d789ba002d424c1d
paiusCatalin <p.cata95@gmail.com>
2021-07-15 17:02:22 +0300

    updated edit domain readonly fields condition

--- code/scripts/view-models/blockchainDomain.js
+++ code/scripts/view-models/blockchainDomain.js
@@ -60,7 +60,7 @@ function getReadOnlyFields(condition) {
         ];
     }
 
-    if (condition === "installing") {
+    if (condition === "installing" || condition === "uninstalling") {
         return [
             "mainDomain",
             "vaultDomain",

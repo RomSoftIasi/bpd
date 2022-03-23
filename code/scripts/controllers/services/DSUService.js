14bc8e69fcbce1c3dd76fb287b911f6c788f156b
Paraschiv-Catalin Paius <p.cata95@gmail.com>
2022-03-23 09:13:42 +0200

    cleanup

--- code/scripts/controllers/services/DSUService.js
+++ code/scripts/controllers/services/DSUService.js
@@ -34,7 +34,6 @@ export default class DSUService {
 
     createDSUAndMount(path, callback) {
         [path, callback] = this._swapParamsIfPathIsMissing(path, callback);
-        console.log("[PATH]", path);
         const templateSSI = keySSISpace.createTemplateSeedSSI(this.DOMAIN);
         resolver.createDSU(templateSSI, (err, dsuInstance) => {
             if (err) {
@@ -49,7 +48,6 @@ export default class DSUService {
                 this.letDSUStorageInit().then(() => {
                     this.DSUStorage.beginBatch();
                     const mountPath = pskPath.join(path, keySSI);
-                    console.log("[PATH]", mountPath);
                     this.DSUStorage.mount(mountPath, keySSI, (err) => {
                         if (err) {
                             return this._cancelBatchOnError(err, callback);
@@ -70,12 +68,7 @@ export default class DSUService {
 
     getEntities(path, callback) {
         [path, callback] = this._swapParamsIfPathIsMissing(path, callback);
-        console.log("[PATH]", path);
         this.letDSUStorageInit().then(() => {
-            this.DSUStorage.listMountedDSUs(path.split("/blockchain-domains")[0], (err, dsuList) => {
-                console.log("[LIST]", err, dsuList);
-            });
-
             this.DSUStorage.listMountedDSUs(path, (err, dsuList) => {
                 if (err) {
                     return callback(err);
@@ -111,9 +104,7 @@ export default class DSUService {
 
     getEntity(uid, path, callback) {
         [path, callback] = this._swapParamsIfPathIsMissing(path, callback);
-        console.log("[PATH]", path);
         const _path = this._getDsuStoragePath(uid, path);
-        console.log("[PATH]", _path);
         this.DSUStorage.getItem(_path, (err, content) => {
             if (err) {
                 return callback(err, undefined);
@@ -126,11 +117,9 @@ export default class DSUService {
 
     updateEntity(entity, path, callback) {
         [path, callback] = this._swapParamsIfPathIsMissing(path, callback);
-        console.log("[PATH]", path);
         this.letDSUStorageInit().then(() => {
             this.DSUStorage.beginBatch();
             const _path = this._getDsuStoragePath(entity.uid, path);
-            console.log("[PATH]", _path);
             this.DSUStorage.setObject(_path, entity, (err) => {
                 if (err) {
                     return this._cancelBatchOnError(err, callback);
@@ -149,9 +138,7 @@ export default class DSUService {
 
     unmountEntity(uid, path, callback) {
         [path, callback] = this._swapParamsIfPathIsMissing(path, callback);
-        console.log("[PATH]", path);
         const unmountPath = pskPath.join(path, uid);
-        console.log("[PATH]", unmountPath);
         this.letDSUStorageInit().then(() => {
             this.DSUStorage.beginBatch();
             this.DSUStorage.unmount(unmountPath, (err) => {
@@ -172,13 +159,11 @@ export default class DSUService {
 
     readFile(path, callback) {
         [path, callback] = this._swapParamsIfPathIsMissing(path, callback);
-        console.log("[PATH]", path);
         this.DSUStorage.readFile(path, callback);
     }
 
     writeFile(path, fileBuffer, callback) {
         [path, callback] = this._swapParamsIfPathIsMissing(path, callback);
-        console.log("[PATH]", path);
         this.letDSUStorageInit().then(() => {
             this.DSUStorage.beginBatch();
             this.DSUStorage.writeFile(path, fileBuffer, (err) => {
